52e11c6fde094645617a9a53cda13de1bab74c07
Sven K <sven@skom.de>
2020-05-17 13:26:15 +0530

    fix: Updated version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,5 +28,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Sven Kalbhenn',
     'author_email' => 'sven@skom.de',
     'author_company' => 'SKom',
-    'version' => '9.0.3',
+    'version' => '9.2.0',
 ];
