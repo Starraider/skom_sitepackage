08cdf898adb2663c44c3761e38e4341d46edb9cd
Sven K <sven@skom.de>
2021-04-04 11:16:40 +0530

    fix(security): Update to secure Version of TYPO3
    Upgrade typo3/cms-core to secure one

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'templates',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.20-10.4.99',
+            'typo3' => '9.5.25-10.4.99',
             'rte_ckeditor' => '8.7.0-10.4.99',
             'bootstrap_package' => '10.0.0-11.99.99'
         ],
