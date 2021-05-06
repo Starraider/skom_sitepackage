dbd0f68c24039ef9fd5c243937a54211ec01966c
Sven K <sven@skom.de>
2021-05-06 18:42:28 +0530

    [FEATURE]: Add commit-lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends: ["@commitlint/config-conventional", "@commitlint/parse"],
+    extends: "@commitlint/config-conventional",
     parserPreset: {
         parserOpts: {
             headerPattern: /^(\[\w*\])?:\(?(\w*)\)?\s(.+)/,
@@ -10,16 +10,19 @@ module.exports = {
         "type-enum": [
             2,
             "always",
-            ["[FEATURE]", "[BUGFIX]", "[DOCS]", "[TASK]", "[SECURITY]"],
+            [
+                "[FEATURE]",
+                "[BUGFIX]",
+                "[DOCS]",
+                "[TASK]",
+                "[SECURITY]",
+                "[RELEASE]",
+            ],
         ],
         "type-case": [2, "always", "upper-case"],
         "type-empty": [2, "never"],
-        "header-max-length": [2, "always", 80],
         "subject-case": [2, "always", "sentence-case"],
         "subject-empty": [2, "never"],
         "subject-full-stop": [2, "never", "."],
-        "body-leading-blank": [2, "always"],
-        "body-max-line-length": [2, "always", 72],
-        "footer-leading-blank": [2, "always"],
     },
 };
